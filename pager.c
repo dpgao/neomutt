0c5ceba6a80d414d62c2c8f23955c118abf315a4
Christopher John CZETTEL <chris@christopher-czettel.net>
2016-09-25 21:50:17 +0200

    ICommand wrapper and hook for new :ex-style interactive commands
    These are needed for the new summary tables feature (ref #162)
    (and iunspired by the Command lookup table in init.c)
    
    added: ICommand wrapper / lookup table allowing easy addition of new
    interactive commands that will not be parsed in .muttrc files and
    override any existing rc-line commands when executed in mutt.
    
    added: hook in mutt_enter_command in command.c to integrate new
    ICommands facility
    
    added :q :qa :q! and :quit commands as proof of concept for new
    ICommands (and because as a heavy vim user, I am always frustrated
    if these commands do not work ;-))
    
    added: stubs for proposed functionality from flatcap (currently
    returning only "Not yet implemented" messages via the ICommand
    error facility.
    
    modified: Makefile.am to include icommands.c and icommands.h
    
    references: #162

--- pager.c
+++ pager.c
@@ -3034,6 +3034,7 @@ int mutt_pager(const char *banner, const char *fname, int flags, struct Pager *e
         old_PagerIndexLines = PagerIndexLines;
 
         mutt_enter_command();
+        pager_menu->redraw = REDRAW_FULL;
 
         if (OptNeedResort)
         {
